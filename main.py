b02a80f331bcba451981b663d37830733c1b6e15
“Jimbob12505” <vatsal.12505@gmail.com>
2024-07-01 11:04:04 +0530

    created project structure and necessary files. Comments added to main.py on how to approach it. Installed openai dependency

---
+++ main.py
@@ -0,0 +1,10 @@
+# Entry point
+
+# method called read_file(filename). Reads the csv file and returns an list[i][j]
+# where list[i] is a row i in the csv file
+
+# method called parse_data(list). Takes in a list of lists returns a list containing
+# either the company_domain or the company name
+
+
+# method called open_ai_request(). Sends an openAI API call
